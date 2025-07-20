5e0388a54d2d0c964be1472305a99421281aa67d
v3xl <v3xl@github.com>
2025-07-20 09:33:17 +0000

    change bind from localhost

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn main() -> std::io::Result<()> {
             .service(web::resource("/").to(index))
             .route("/smtp/send", web::post().to(send_email_endpoint))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
